b2124a0051da29b365319c1cee964bc7c2873532
Néstor A. M <alvarado.nestor@yandex.com>
2017-07-05 23:24:44 -0500

    Terminé el test de funciones

---
+++ functionsTest.js
@@ -0,0 +1,34 @@
+function isEven(num){
+    if(typeof num === "number"){
+        if(num % 2 === 0){
+            return true;
+        }
+        return false;
+    }
+    else{
+        console.log(num + " no es un numero");
+    }
+}
+
+function factorial(num){
+     if(typeof num === "number"){
+        var total = 1;
+        for(var i = 1; i <= num; i++){
+            total *= i;
+        }
+        return total;
+     }
+     else{
+         console.log(num + " no es un numero");
+     }
+    
+}
+
+function kebabToSnake(cadena){
+    if(typeof cadena === "string"){
+        return cadena.replace("-", "_");
+    }
+    else{
+         console.log(num + " no es una cadena");
+    }
+}
